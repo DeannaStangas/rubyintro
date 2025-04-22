9d5d3b577439df72d698e79bfbb4fcfaa1b062a0
Deanna Stangas <deanna.stangas@kellogg.northwestern.edu>
2025-04-22 16:29:49 +0000

    final change

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -3,9 +3,26 @@
 # ruby 5-loops.rb
 
 # Infinite Tacos -> Finite Tacos
+# index=0
+# loop do
+#     puts "tacos!"
+#     if index ==10
+#         break
+#     end
+#     index=index+1
+# end
 # loop do
 #   puts "tacos!"
 # end
 
 # Loop through tacos
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+index=0
+loop do
+    if index == tacos.length
+        break
+    end
+    puts tacos[index]
+    index=index+1
+end
 # tacos = ["carnitas", "carne asada", "pollo", "pescado"]
